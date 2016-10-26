fd1ca4d10dda18f389c4af4d1ded66b6b887492f
Jung <vj.elfins@gmail.com>
2016-10-26 11:28:51 +0200

    Initial commit

---
+++ Greetings.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Peaq on 26-10-2016.
+ */
+public class Greetings {
+    public static void main(String[] args) {
+        System.out.println("Greetings Earthlings, resistance is futile");
+    }
+}
