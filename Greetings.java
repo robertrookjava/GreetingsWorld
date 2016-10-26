d645a6c146612bad53cac6396927b7aa88b7b1a1
Jung <JavaJung@users.noreply.github.com>
2016-10-26 11:30:37 +0200

    First branch

--- Greetings.java
+++ Greetings.java
@@ -4,5 +4,6 @@
 public class Greetings {
     public static void main(String[] args) {
         System.out.println("Greetings Earthlings, resistance is futile");
+        System.out.println("We come in peace, shoot to kill.");
     }
 }
